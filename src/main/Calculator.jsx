757905186dc8cb138203432bc8b0a1db3ae8f059
Tiago Moreira <tsm_projetos@hotmail.com>
2020-03-19 23:20:17 -0300

    implementado: metodos com logica dos calculos de soma, subtração, divisão e multiplicação

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,8 +4,18 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initicalState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
     
+    state = {...initicalState}
+
     constructor (props) {
         super(props)
 
@@ -15,27 +25,82 @@ export default class Calculator extends Component {
     }
 
     clearMemory () {
-        console.log ('limpar')
+        this.setState({ ...initicalState })
     }
 
     setOperation (operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+
+            switch (currentOperation) {
+                case '÷':
+                    values[0] = values[0] / values[1]
+                    break
+                case 'x':
+                    values[0] = values[0] * values[1]
+                    break
+                case '-':
+                    values[0] = values[0] - values[1]
+                    break
+                case '+':
+                    values[0] = values[0] + values[1]
+                    break
+                default:
+                    console.log('Operador matemático inválido')
+
+            }
+            
+            values[1] = 0
+
+            this.setState ({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        //false ou true
+        const clearDisplay = 
+            this.state.displayValue === '0' || this.state.clearDisplay   
+
+        const currentValue = 
+            clearDisplay ? '' : this.state.displayValue        
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render () {
         return (
             <div className="calculator">
-                <Display value={1000}></Display>
+                <Display value={this.state.displayValue}></Display>
                 <Button label="AC" click={this.clearMemory} triple></Button>
-                <Button label="/" click={this.setOperation} operation></Button>
+                <Button label="÷" click={this.setOperation} operation></Button>
                 <Button label="7" click={this.addDigit}></Button>
                 <Button label="8" click={this.addDigit}></Button>
                 <Button label="9" click={this.addDigit}></Button>
-                <Button label="*" click={this.setOperation} operation></Button>
+                <Button label="x" click={this.setOperation} operation></Button>
                 <Button label="4" click={this.addDigit}></Button>
                 <Button label="5" click={this.addDigit}></Button>
                 <Button label="6" click={this.addDigit}></Button>
