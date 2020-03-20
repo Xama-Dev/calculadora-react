4cc7bb5b1a3c26628fc9329ce4d39bd98026f5fd
Tiago Moreira <tsm_projetos@hotmail.com>
2020-03-19 23:24:04 -0300

    implementado: metodos com logica dos calculos de soma, subtração, divisão e multiplicação

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -87,7 +87,6 @@ export default class Calculator extends Component {
             const values = [...this.state.values]
             values[i] = newValue
             this.setState({ values })
-            console.log(values)
         }
     }
 
